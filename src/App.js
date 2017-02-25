27665df4be7255360306df58272894d079f98419
Nithun Harikrishnan <nithun@gmail.com>
2017-02-25 02:22:52 -0600

    App skeleton complete

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
       <h1>React Router Tutorial</h1>
       <ul role="nav">
         <li><Link to="/home">Home</Link></li>
-        <li><Link to="/login">Login</Link></li>
+        <li><Link to="/about">About</Link></li>
       </ul>
       {this.props.children}
       <h1>React Router Tutorial</h1>
--- src/index.js
+++ src/index.js
@@ -4,15 +4,24 @@ import App from './App';
 import Login from './components/Login';
 import Home from './components/Home';
 import About from './components/About';
-import { Router, Route, hashHistory } from 'react-router'
+import Dash from './components/Dash';
+import Appoinments from './components/Appoinments';
+import Files from './components/Files';
+import Messages from './components/Messages';
+import NotFound from './components/NotFound';
+import { Router, Route, browserHistory,IndexRoute  } from 'react-router'
 
-
+// TODO: Change Home to App and Dash to home
 ReactDOM.render((
-  <Router history={hashHistory}>
-    <Route path="/" component={App}>
-    <Route path="/home" component={Home}/>
-    <Route path="/login" component={Login}/>
+  <Router history={browserHistory}>
+  <Route path="/" component={Login}/>
+    <Route path="/home" component={Home}>
+      <IndexRoute component={Dash}/>
+    <Route path="/appoinments" component={Appoinments}/>
+    <Route path="/files" component={Files}/>
+    <Route path="/messages" component={Messages}/>
     <Route path="/about" component={About}/>
     </Route>
+    <Route path="*" component={NotFound} />
   </Router>
 ), document.getElementById('root'))
